923050a98a7a8d61f0c3fd3de562c5e5bf2dd1e1
James P. Galovic <jpgalovic@gmail.com>
2025-03-07 03:42:22 +1100

    Trying to get this to work

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,16 +1,2 @@
-interface ContextRenderer {
-    (
-        conent: string | Promise<string>,
-        props: { title: string },
-    );
-}
 
-interface Env {
-    Variables: {
-
-    };
-    Bindings: {
-        DB: D1Database;
-    }
-}
 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,34 @@
 import { Hono } from "hono";
+import { jsxRenderer } from "hono/jsx-renderer";
+import { Footer } from "./componants/footer";
+
+declare module 'hono' {
+    interface ContextRenderer {
+        (
+            conent: string | Promise<string>,
+            props: { title: string },
+        ): Response
+    }
+}
+
+interface Env {
+    Variables: {
+
+    };
+    Bindings: {
+        DB: D1Database;
+    }
+}
 
 const app = new Hono();
 
-app.get("/", (c) => {
-    return c.render("Hello World")
+app.get('/', (c) => {
+    return c.render(
+      "<div><ul><li>Eating sushi</li><li>Watching baseball games</li></ul></div>",
+      {
+        title: 'My favorites',
+      }
+    )
   })
 
 export default app;
