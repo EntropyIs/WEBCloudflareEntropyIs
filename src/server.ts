1a4461b008ceb3e8e9dffaa5e1daafdd5ad67c59
James P. Galovic <jpgalovic@gmail.com>
2025-03-07 03:29:23 +1100

    Add Test String

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
-import { showRoutes } from "hono/dev";
 
 const app = new Hono();
 
-showRoutes(app)
+app.get("/", (c) => {
+    return c.render("Hello World")
+  })
 
 export default app;
 
